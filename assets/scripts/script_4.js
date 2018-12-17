b4c67814d88336e891ce0f7f9f98bff16db05fb9
mmits <44256454+mmits@users.noreply.github.com>
2018-12-17 21:45:41 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -5,7 +5,7 @@ var margin = {top: 20, right: 160, bottom: 35, left: 30};
 var width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#body")
+var svg = d3.select("#stacked")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
