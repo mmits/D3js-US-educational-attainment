3ad3eefd35cd218a98a93713ceab305cd4c68f9b
mmits <44256454+mmits@users.noreply.github.com>
2018-12-21 15:59:44 +0200

    Update script_4.js

--- assets/scripts/script_4.js
+++ assets/scripts/script_4.js
@@ -10,7 +10,7 @@ var svg = d3.select("#stacked")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
    .call(stacked);
 
 
